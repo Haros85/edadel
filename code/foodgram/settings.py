7adba6eb2abc95cfa7b758a4e6227afea1312aa6
Haros85 <ruslan@antonenko.me>
2020-12-25 22:48:42 +1000

    change db

--- code/foodgram/settings.py
+++ code/foodgram/settings.py
@@ -86,7 +86,6 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-""" 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -105,7 +104,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
