35d6db808b01f4fe43d2a3a25988722bf95ba083
Haros85 <ruslan@antonenko.me>
2020-12-30 13:45:35 +1000

    add ingredietns bug fix

--- code/recipe/forms.py
+++ code/recipe/forms.py
@@ -1,5 +1,6 @@
 from django.forms import CheckboxSelectMultiple, ModelForm
-from recipe.models import Recipe
+from django.core.exceptions import ValidationError
+from recipe.models import Recipe, Ingredient
 
 
 class RecipeForm(ModelForm):
@@ -9,3 +10,19 @@ class RecipeForm(ModelForm):
         widgets = {
             'tag': CheckboxSelectMultiple(),
         }
+
+    def clean(self):
+        known_ids = []
+        for items in self.data.keys():
+            if 'nameIngredient' in items:
+                name, id = items.split('_')
+                known_ids.append(id)
+        for id in known_ids:
+            title = self.data.get(f'nameIngredient_{id}'),
+            unit = self.data.get(f'unitsIngredient_{id}')
+            ingredient_exists = Ingredient.objects.filter(
+                title=title[0],
+                unit=unit,
+            ).exists()
+            if not ingredient_exists:
+                raise ValidationError('Выберите ингредиент из списка!')
